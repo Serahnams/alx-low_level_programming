9a28de0ea826e199a6b90a0df2f3a834848623b3
Serahnams <serahnams@gmail.com>
2023-04-27 12:53:11 -0700

    More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: integer to be printed
+ */
+void print_number(int n)
+{
+	unsigned int q;
+
+	if (n < 0)
+	{
+		q = -n;
+		_putchar('-');
+	} else
+	{
+		q = n;
+	}
+
+	if (q / 10)
+	{
+		print_number(q / 10);
+	}
+
+	_putchar((q % 10) + '0');
+}
+
