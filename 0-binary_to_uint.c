aa1742f9519dc984c94cb6371a68755607a473bb
Serahnams <serahnams@gmail.com>
2023-07-06 15:21:49 -0700

    Task 0. 0

--- 0-binary_to_uint.c
+++ 0-binary_to_uint.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stddef.h>
+
 /**
 * binary_to_uint - binary number converted to unsigned int
 * @b: string of chars
@@ -8,15 +9,18 @@
 */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int con = 0;
-if (b == NULL)
-return (0);
-while (*b != '\0')
-{
-if (*b != '0' && *b != '1')
-return (0);
-con = (con << 1) + (*b - '0');
-b++;
-}
-return (con);
+	unsigned int con = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (*b != '\0')
+	{
+		if (*b != '0' && *b != '1')
+			return (0);
+
+		con = (con << 1) + (*b - '0');
+		b++;
+	}
+	return (con);
 }
