1b6ff9871040add4978010b0ba0c9fd970bfaedf
Serahnams <serahnams@gmail.com>
2023-07-10 17:40:22 -0700

    cp

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,5 +11,6 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 void close_file(int fd, char *filename);
 int main(int argc, char *argv[]);
+int _putchar(char c);
 
 #endif
